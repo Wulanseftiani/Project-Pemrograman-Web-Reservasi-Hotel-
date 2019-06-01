78ebcd9af22dd1de2fb5f6f98e1c14b4ba05b637
Wulan Seftiani <38196197+Wulanseftiani@users.noreply.github.com>
2019-06-01 19:32:47 +0700

    Add files via upload

---
+++ pilihankamar.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <title>Pilihan Kamar</title>
+        <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+        <link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+    </head>
+<body>
+     <style>
+        body{
+        background-image:url(hotel7.jpg);
+            background-size:cover;
+            background-attachment: fixed;
+                }
+        p{
+            color:white;
+        }
+    </style>
+    <center>
+    <nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+        <div  class = "container-fluid">
+            <div class = "navbar-header">
+                <a class = "navbar-brand" >Pilihan Kamar</a>
+            </div>
+        </div>
+    </nav>  
+                <br />
+                <hr style = "border:1px dotted #000;" />
+                <br />
+                <div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+                    <center><img src = "standar.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+                    <center><h4 style = "color:rgba(0, 255, 0, 1);">Standar Room</h4></center>
+                    <center><label style = "color:white;">Standar Room</label> <label style = "color:red;">200.000 IDR</label></center>
+                </div>
+                <div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+                    <center><img src = "superior.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+                    <center><h4 style = "color:rgba(0, 255, 0, 1);">Superior Room</h4></center>
+                    <center><label style = "color:white;">Superior Room</label> <label style = "color:red;">300.000 IDR</label></center>
+                </div>
+                <div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+                    <center><img src = "deluxe.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+                    <center><h4 style = "color:rgba(0, 255, 0, 1);">Deluxe Room</h4></center>
+                    <center><label style = "color:white;">Deluxe Room</label> <label style = "color:red;">500.000 IDR </label></center>
+                </div>
+                <br style = "clear:both;"/>
+                <br />
+                 <br />
+                <div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+                    <center><img src = "suite.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+                    <center><h4 style = "color:rgba(0, 255, 0, 1);">Suite Room</h4></center>
+                    <center><label style = "color:white;">Suite Room</label> <label style = "color:red;">1.000.000 IDR </label></center>
+                </div>
+                <div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+                    <center><img src = "presidential.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+                    <center><h4 style = "color:rgba(0, 255, 0, 1);">Presidential Room</h4></center>
+                    <center><label style = "color:white;">Presidential Room</label> <label style = "color:red;">1.300.000 IDR </label></center>
+                </div>
+                <br style = "clear:both;"/>
+                <br />
+                <br />
+            </div>
+        </div>
+    </div>
+    <br />
+    <br />
+    <div style = "text-align:right; margin-right:10px;" class = "navbar navbar-default navbar-fixed-bottom">
+        <label>&copy; Copyright STMIK 2K17 </label>
+    </div>
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>  
+</html> 
