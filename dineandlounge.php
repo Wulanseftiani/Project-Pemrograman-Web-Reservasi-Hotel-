0966b7f4138303fe441bea9fdd032f16829a8ee3
Wulan Seftiani <38196197+Wulanseftiani@users.noreply.github.com>
2019-06-01 19:25:42 +0700

    Add files via upload

---
+++ dineandlounge.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+        <link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+    </head>
+<body>
+    <center>
+          <style>
+        body{
+        background-image:url(BG3.jpg);
+            background-size:cover;
+            background-attachment: fixed;
+                }
+        p{
+            color:white;
+        }
+    </style>
+    <center>
+    <nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+        <div  class = "container-fluid">
+            <div class = "navbar-header">
+            </div>
+        </div>
+    </nav>   
+    <div style = "margin-left:0;" class = "container">
+        <div class = "panel panel-default">
+            <div class = "panel-body">
+                <center>
+                <strong><h3>DINE AND LOUNGE</h3></strong>
+                <br />
+                <br />
+                <div style = "float:left; width:250px; height:250px; margin-left:20px;">
+                    <img src = "mkn1.jpg" width = "250" height = "250"/>
+                </div>
+                <div style = "float:left; width:250px; height:250px; margin-left:20px;">
+                    <img src = "mkn2.jpg" width = "250" height = "250"/>
+                </div>
+                <div style = "float:left; width:250px; height:250px; margin-left:20px;">
+                    <img src = "mkn3.jpg" width = "250" height = "250"/>
+                </div>
+                <div style = "float:left; width:250px; height:250px; margin-left:20px;">
+                    <img src = "mkn4.jpg" width = "250" height = "250"/>
+                </div>
+                <br style = "clear:both;" />
+                <br />
+                <div style = "float:left; width:250px; height:250px; margin-left:20px;">
+                    <img src = "mkn5.jpg" width = "250" height = "250"/>
+                </div>
+                <div style = "float:left; width:250px; height:250px; margin-left:20px;">
+                    <img src = "mkn6.jpg" width = "250" height = "250"/>
+                </div>
+                <div style = "float:left; width:250px; height:250px; margin-left:20px;">
+                    <img src = "mkn7.jpg" width = "250" height = "250"/>
+                </div>
+                <div style = "float:left; width:250px; height:250px; margin-left:20px;">
+                    <img src = "mkn8.jpg" width = "250" height = "250"/>
+                </div>
+                <br style = "clear:both;" />
+                <br />
+                <div style = "float:left; width:250px; height:250px; margin-left:20px;">
+                    <img src = "mkn9.jpg" width = "250" height = "250"/>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br />
+    <br />
+    <div style = "text-align:right; margin-right:10px;" class = "navbar navbar-default navbar-fixed-bottom">
+        <label>&copy; Copyright STMIK 2K17 </label>
+    </div>
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>   
+</html>
