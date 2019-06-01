619412e4e3e0099d0d5cc906d8297cd3980b8684
Wulan Seftiani <38196197+Wulanseftiani@users.noreply.github.com>
2019-06-01 19:26:11 +0700

    Add files via upload

---
+++ fasilitas.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+     <font color="white" size="5">
+        <strong><title>Fasilitas</title> </strong>
+                <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+        <link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+          </strong>
+    </head>
+    <body>
+     <style>
+        body{
+        background-image:url(hotel7.jpg);
+            background-size:cover;
+            background-attachment: fixed;
+                }
+        p{
+            color:white;
+        }
+    </style>
+    <center>
+    <nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+        <div  class = "container-fluid">
+            <div class = "navbar-header">
+                <a class = "navbar-brand" >Fasilitas</a>
+            </div>
+        </div>
+    </nav>  
+<ul>
+                    <li><label>Layanan Kamar 24 jam</label></li>
+                    <li><label>21 "TV layar datar dengan layanan kabel</label></li>
+                    <li><label>Mandi air panas & dingin</label></li>
+                    <li><label>Kulkas dan mini bar sesuai permintaan di semua suite dan kamar superior</label></li>
+                    <li><label>Set kopi & teh, air kemasan, roti organik di suite dan kamar superior </label></li>
+                    <li><label>Pengering rambut di kamar suite</label></li>
+                    <li><label>Kotak pengaman pribadi di setiap ruangan</label></li>
+                    <li><label>Laundry & pressing</label></li>
+                    <li><label>Gratis Wifi</label></li>
+                    <li><label>Layanan Pijat</label></li>
+                    <li><label>Aman Pribadi di Setiap Kamar</label></li>
+                    <li><label>Mini Bar</label></li>
+                    <li><label>7 Ruang Fungsi & Rapat</label></li>
+                    <li><label>Road Trip Transfer Bandara & Wisata Kota Khusus</label></li>
+                    <li><label>Kolam Renang</label></li>
+                    <li><label>Toko Hadiah</label></li>
+                    <li><label>Business Center</label></li>
+                    <li><label>Parkir gratis untuk tamu</label></li>
+                </ul>
