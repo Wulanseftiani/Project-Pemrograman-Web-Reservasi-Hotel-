453e9b319f3fc41c3298cb24c464e163fea3cf76
Wulan Seftiani <38196197+Wulanseftiani@users.noreply.github.com>
2019-06-01 19:34:09 +0700

    Add files via upload

---
+++ reply_reserve.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+        <link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+    </head>
+<body>
+    <style>
+        body{
+        background-image:url(hotel7.jpg);
+            background-size:cover;
+            background-attachment: fixed;
+                }
+        p{
+            color:white;
+        }
+    </style>
+    <nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+        <div  class = "container-fluid">
+            <div class = "navbar-header">
+            </div>
+        </div>
+    </nav>   
+    
+    <div style = "margin-left:0;" class = "container">
+        <div class = "panel panel-default">
+            <div class = "panel-body">
+                <br />
+                <div class = "col-md-4"></div>
+                <div class = "well col-md-4">
+                    <font color="white" size="3">
+                    <center><h3>Silahkan kunjungi Hotel kami untuk verifikasi</h3></center>
+                    <br />
+                    <center><h4>TERIMA KASIH!</h4></center>
+                    <br />
+                    <font color="white" size="3">
+                    <center><a href = "menu.html" class = "btn btn-success"><i class = "glphyicon glyphicon-check"></i> Kembali ke menu</a></center>
+                </div>
+                <div class = "col-md-4"></div>
+            </div>
+        </div>
+    </div>
+    <br />
+    <br />
+    </div>
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>   
+</html>
