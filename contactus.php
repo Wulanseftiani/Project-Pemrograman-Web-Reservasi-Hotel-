1f7a050a857cd1bda07d98f68ba2b0cb809c1574
Wulan Seftiani <38196197+Wulanseftiani@users.noreply.github.com>
2019-06-01 19:24:35 +0700

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+        <link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+    </head>
+<body>
+     <style>
+        body{
+        background-image:url(hotel7.jpg);
+            background-size:cover;
+            background-attachment: fixed;
+                }
+        p{
+            color:white;
+        }
+    </style>
+    <center>
+    <nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+        <div  class = "container-fluid">
+            <div class = "navbar-header">
+            </div>
+        </div>
+    </nav>   
+    <div style = "margin-left:0;" class = "container">
+        <div class = "panel panel-default">
+            <div class = "panel-body">
+                 <font color="white" size="3">
+                <strong><h3>KONTAK KAMI</h3></strong>
+                <br />
+                <br />
+                <center><img src = "wa.jpg" width = "200" height = "200" /></center>
+                <br />
+                <br />
+                <center>
+                <p>Contact No: +621323456789</p>
+                <p>Email: rooms@hotel.com</p>
+                </center>
+            </div>
+        </div>
+    </div>
+    <br />
+    <br />
+    </div>
+</body>  
+</html>
